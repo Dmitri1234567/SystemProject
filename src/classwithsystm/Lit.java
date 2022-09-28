cc705b599fc8c8cd1260618db22d8381477350f0
Dima <dmitrijskurcenko3@gmail.com>
2022-09-28 14:04:18 +0300

    SystemProject
    SystemProject

---
+++ src/classwithsystm/Lit.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classwithsystm;
+
+/**
+ *
+ * @author admin
+ */
+public class Lit {
+     private int a;
+
+    public Lit(int a) {
+        this.a = a;
+    }
+    public void show(){
+    System.out.print("+++");
+    System.out.println(" ");
+    System.out.print("!!!!");
+    System.out.println("  ");
+    System.out.println("@@@@@");
+    System.out.println(a);
+     System.out.println(a*2);
+      System.out.println(a*3);
+    
+    }
+   
+    
+    
+}
